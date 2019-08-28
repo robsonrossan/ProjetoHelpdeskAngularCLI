d72e1db67022f728ac8431d605c965ecaf95a857
Robson dos Santos <robsonrossan@hotmail.com>
2019-08-28 18:33:58 -0300

    classes de servicos: User e Ticket

---
+++ src/app/services/ticket.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Ticket } from '../model/ticket.model';
+import{HELP_DESK_API} from './helpdesk.api';
+
+@Injectable()
+export class TicketService {
+
+  constructor(private http: HttpClient) { }
+
+  createOrUpdate(ticket: Ticket){
+    if(ticket.id != null && ticket.id != ''){
+      return this.http.put(`${HELP_DESK_API}/api/ticket`, ticket);
+    }else{
+      ticket.id = null;
+      ticket.status = 'New';
+      return this.http.post(`${HELP_DESK_API}/api/ticket`, ticket);
+    }
+  }
+
+  findAll(page:number, count:number){
+    return this.http.get(`${HELP_DESK_API}/api/ticket/${page}/${count}`);
+  }
+
+  findById(id:string){
+    return this.http.get(`${HELP_DESK_API}/api/ticket/${id}`);
+  }
+
+  delete(id:string){
+    return this.http.delete(`${HELP_DESK_API}/api/ticket/${id}`);
+  }
+
+  findByparams(page:number, count:number, assignedToMe:boolean, t:Ticket){
+    t.number = t.number == null ? 0 : t.number; 
+    t.title = t.title == '' ? 'uninformed' : t.title; // if ternario: verificando se o titulo esta vazio. se estiver seta "uninformed"
+    t.status = t.status == '' ? 'uninformed' : t.status;
+    t.priority = t.priority == '' ? 'uninformed' : t.priority;
+    return this.http.get(`${HELP_DESK_API}/api/ticket/${page}/${count}/${t.number}/${t.status}/${t.priority}/${assignedToMe}/`);
+  }
+
+  chargeStatus(status:string, ticket:Ticket){
+    return this.http.put(`${HELP_DESK_API}/api/ticket/${ticket.id}/${status}`, ticket);
+  }
+
+  summary(){
+    return this.http.get(`${HELP_DESK_API}/api/ticket/summary`); 
+  }
+}
