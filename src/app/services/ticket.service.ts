5696b2ce32ccc125d4dea0398f151f1c7cc1a283
Robson dos Santos <robsonrossan@hotmail.com>
2019-10-08 11:13:48 -0300

    Alterações

--- src/app/services/ticket.service.ts
+++ src/app/services/ticket.service.ts
@@ -6,20 +6,19 @@ import{HELP_DESK_API} from './helpdesk.api';
 @Injectable()
 export class TicketService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   createOrUpdate(ticket: Ticket){
     if(ticket.id != null && ticket.id != ''){
-      return this.http.put(`${HELP_DESK_API}/api/ticket`, ticket);
-    }else{
-      console.log(ticket);
+      return this.http.put(`${HELP_DESK_API}/api/ticket`,ticket);
+    } else {
       ticket.id = null;
       ticket.status = 'New';
       return this.http.post(`${HELP_DESK_API}/api/ticket`, ticket);
     }
   }
 
-  findAll(page:number, count:number){
+  findAll(page:number,count:number){
     return this.http.get(`${HELP_DESK_API}/api/ticket/${page}/${count}`);
   }
 
@@ -31,19 +30,20 @@ export class TicketService {
     return this.http.delete(`${HELP_DESK_API}/api/ticket/${id}`);
   }
 
-  findByParams(page:number, count:number, assignedToMe:boolean, t:Ticket){
-    t.number = t.number == null ? 0 : t.number; 
-    t.title = t.title == '' ? 'uninformed' : t.title; // if ternario: verificando se o titulo esta vazio. se estiver seta "uninformed"
-    t.status = t.status == '' ? 'uninformed' : t.status;
-    t.priority = t.priority == '' ? 'uninformed' : t.priority;
-    return this.http.get(`${HELP_DESK_API}/api/ticket/${page}/${count}/${t.number}/${t.status}/${t.priority}/${assignedToMe}/`);
+  findByParams(page:number,count:number,assignedToMe:boolean,t:Ticket){
+    t.number = t.number == null ? 0 : t.number;
+    t.title = t.title == '' ? "uninformed" : t.title;
+    t.status = t.status == '' ? "uninformed" : t.status;
+    t.priority = t.priority == '' ? "uninformed" : t.priority;
+    return this.http.get(`${HELP_DESK_API}/api/ticket/${page}/${count}/${t.number}/${t.title}/${t.status}/${t.priority}/${assignedToMe}`);
   }
 
-  changeStatus(status:string, ticket:Ticket){
-    return this.http.put(`${HELP_DESK_API}/api/ticket/${ticket.id}/${status}`, ticket);
+  changeStatus(status:string,ticket:Ticket){
+    return this.http.put(`${HELP_DESK_API}/api/ticket/${ticket.id}/${status}`,ticket);
   }
 
   summary(){
-    return this.http.get(`${HELP_DESK_API}/api/ticket/summary`); 
+    return this.http.get(`${HELP_DESK_API}/api/ticket/summary`);
   }
+
 }
